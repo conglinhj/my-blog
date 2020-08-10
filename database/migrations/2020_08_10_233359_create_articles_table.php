c8736b4703d7a74338d41f45fad4b3839705a05a
LinhNC <nclinh184@gmail.com>
2020-08-11 00:41:16 +0700

    Initial project

---
+++ database/migrations/2020_08_10_233359_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('description');
+            $table->longText('content');
+            $table->timestampsTz();
+            $table->softDeletesTz('deleted_at', 0);
+            $table->foreignId('author_id')->constrained('users');
+            $table->foreignId('category_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
---
+++ database/migrations/2020_08_11_001943_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('parent_id');
+            $table->mediumText('content');
+            $table->timestampsTz();
+            $table->softDeletesTz('deleted_at', 0);
+            $table->foreignId('author_id')->constrained('users');
+            $table->foreignId('article_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
