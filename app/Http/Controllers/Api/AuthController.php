c1a9c0983d3e407373b35d0cb639c17dae3489a6
LinhNC <nclinh184@gmail.com>
2020-08-24 00:27:09 +0700

    #2 - api authentication

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
+
+
+class AuthController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $user->tokens()->where('name', $request->device_name)->delete();
+        $new_token = $user->createToken($request->device_name)->plainTextToken;
+
+        return new JsonResponse([ 'access_token' => $new_token ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        $user = new User(
+            $request->only([
+                'name',
+                'email',
+                'password'
+            ])
+        );
+        $user->save();
+
+        return new JsonResponse([
+            'data' => $user,
+            'access_token' => $user->createToken($request->device_name ?? 'token')->plainTextToken
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return void
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+    }
+}
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -44,7 +44,7 @@ class Comment extends Model
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function author() {
-        return $this->belongsTo('App\User', 'author_id');
+        return $this->belongsTo('App\Models\User', 'author_id');
     }
 
     /**
