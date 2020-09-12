faa2c28700264e4c22b8f45249fe14f158ca9f94
LinhNC <nclinh184@gmail.com>
2020-09-12 16:25:09 +0700

    fix category vaidation rules

--- app/Http/Requests/CategoryUpdateRequest.php
+++ app/Http/Requests/CategoryUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class CategoryUpdateRequest extends FormRequest
 {
@@ -24,7 +25,7 @@ class CategoryUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
+            'name' => ['required', Rule::unique('categories')->ignore($this->category)],
             'parent_id' => "not_in:{$this->category}|exists:categories,id"
         ];
     }
